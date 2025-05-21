39e5549041cabd1eaf26c69daf98e1a8804f9524
Prince Lucban <116994228+PrinceJL@users.noreply.github.com>
2025-05-21 20:40:03 +0800

    Made the allowable origin to all public

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=["*"], #Dont do this
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
